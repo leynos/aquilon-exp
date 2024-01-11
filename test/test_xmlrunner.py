ce8115fdc48b48691917ba68b358ff63bc756b40
Cedric Charest <Cedric.Charest@morganstanley.com>
2024-01-11 13:22:13 -0500

    AQUILONAQD-2092 Add EDM support to command ran by Selfservice API
    * Ran ruff on updated py file
    
    * Exposed comments for reboot_intervention
    
    * Added unittest

--- test/test_xmlrunner.py
+++ test/test_xmlrunner.py
@@ -1,30 +1,31 @@
-#!/ms/dist/python/PROJ/core/2.7.18-64/bin/python
+#!/ms/dist/python/PROJ/core/3.7.9-1/bin/python
 
 # Test runner with support for coverage and xml test outout
 
 # Avoid polluting general dependencies with test specific libs,
 # only pull it here
 import ms.version
+
 ms.version.addpkg("unittest-xml-reporting", "2.5.1")
-ms.version.addpkg("setuptools", "41.0.1")
-ms.version.addpkg("six", "1.14.0")
+ms.version.addpkg("setuptools", "45.0.0")
+ms.version.addpkg("six", "1.16.0")
 
 import os
 import sys
 import unittest
+
 import xmlrunner
 
 # Output file path is relative to the current directory. As we're expected to be run by
 # "train test", that would be the src/ folder (and not the tests/ subdirectory!)
 DEFAULT_OUTPUT = "../install/common/test-results/unit"
 
-BASEDIR = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+BASEDIR = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(BASEDIR)
 
-import dependency
 
-from commands import test_compile, test_search_audit, test_compile_hostname, test_del_host
-from dbwrappers import test_location, test_change_management
+from commands import test_compile, test_compile_hostname, test_del_host, test_search_audit
+from dbwrappers import test_change_management, test_location
 from formats import test_location
 from templates import test_domain
 
@@ -44,11 +45,9 @@ suite = unittest.TestSuite()
 
 for module in modules_to_test:
     suite.addTests(loader.loadTestsFromModule(module))
-out_file = os.path.join(BASEDIR,
-                            '../install/common/test-results/'
-                            'UNIT/xml/test.xml')
-with open(out_file, 'wb') as output:
+out_file = os.path.join(BASEDIR, "../install/common/test-results/UNIT/xml/test.xml")
+with open(out_file, "wb") as output:
     results = xmlrunner.XMLTestRunner(output=output).run(suite)
 
 if results.failures or results.errors:
-    exit(-1)
+    sys.exit(-1)
