c72c639b42773c2e9b89229ea79eeb008082d2c8
Karthik Kamurthy <Karthik.Kamurthy@morganstanley.com>
2024-06-18 14:12:08 -0400

    AQUILONAQD-2472/FIX-COMMAND-INJECTION-VULNERABILITIES-ON-AQD
    * Fixed the command injection vulnerabilities reported by snyk tool.
    * Added aqweb process to be restarted as part of train-deploy when
    deployment is done.
    * Fixed test cases which were failing due to change in templates.

--- tests/broker/test_search_resource.py
+++ tests/broker/test_search_resource.py
@@ -110,7 +110,7 @@ class TestSearchResource(TestBrokerCommand):
     def test_030_search_intervention(self):
         command = ['search_intervention', '--hostname', 'server1.aqd-unittest.ms.com']
         out = self.commandtest(command)
-        self.matchoutput(out, 'Intervention: i1', command)
+        self.matchoutput(out, 'Intervention: i1234', command)
         self.matchoutput(out, 'Intervention: blank', command)
         self.matchoutput(out, 'Intervention: groups', command)
         self.matchoutput(out, 'Intervention: disable', command)
